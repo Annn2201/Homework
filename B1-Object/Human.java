54f8bcdca9f77d3b9990c814e2107e0842b1a1f5
Annn2201 <113761537+Annn2201@users.noreply.github.com>
2022-09-26 10:11:19 +0700

    Add files via upload

---
+++ B1-Object/Human.java
@@ -0,0 +1,19 @@
+
+
+public class Human 
+{
+    static void walk()
+    {
+        System.out.println("i'm walking");
+    }   
+    static void eat()
+    {
+        System.out.println("i'm eating");
+    }
+    public static void main(String[] args) 
+    {
+        Human Humen = new Human();
+        Humen.walk();
+        Humen.eat();    
+    }   
+}
