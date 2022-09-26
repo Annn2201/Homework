26b7fc94a75aa055c2b21622a05e7c27465d805a
Annn2201 <113761537+Annn2201@users.noreply.github.com>
2022-09-26 12:26:01 +0700

    Add files via upload

---
+++ B2-OOP create object/Book.java
@@ -0,0 +1,67 @@
+//package B2-OOP create object;
+
+public class Book 
+{
+    private String name;
+    private Author author;
+    private double price;
+    private int qty = 0;
+
+    public Book(String name, Author author, double price)
+    {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(String name, Author author, double price, int qty)
+    {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        qty = 0;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public Author getAuthor()
+    {
+        return author;
+    }
+
+    public double getPrice() 
+    {
+        return price;
+    }
+
+    public void setPrice(double p)
+    {
+        price = p;
+    }
+
+    public int getQty()
+    {
+        return qty;
+    }
+
+    public void setQty(int q)
+    {
+        qty = q;
+    }
+
+    public String toString() {
+        String str = "Book: " + name + " " + author + " Price = " + price + " Qty = " + qty;
+        return str;
+    }
+
+    public static void main(String[] args) 
+    {
+        Author author = new Author("An", "an123@gmail.com", 'm');
+        Book book = new Book("tu chuyen", author, 100);
+        Book book1 = new Book("tu chuyen", author, 100, 5);
+        System.out.println(book);
+    }
+}
