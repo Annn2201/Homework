a6bf7d624713a91d83846839205568e301eecd1e
Annn2201 <113761537+Annn2201@users.noreply.github.com>
2022-09-26 10:40:33 +0700

    Add files via upload

---
+++ B2-OOP create object/Author.java
@@ -0,0 +1,41 @@
+
+public class Author 
+{
+    private String name;
+    private String email;
+    private char gender;
+
+    public Author (String name, String email, char gender)
+    {
+        name = this.name;
+        email = this.email;
+        gender = this.gender;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail (String e)
+    {
+        email = e;   
+    }
+
+    public char getGender()
+    {
+        return gender;
+    }
+
+    public String toString()
+    {
+        String str = "Author: " + name + 
+                    "Email: " + email + "Gender: " + gender;
+        return str;
+    }
+}
