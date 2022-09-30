ac0ed57bfcfae3b50ef31bc7f04329df0d06ed43
Annn2201 <113761537+Annn2201@users.noreply.github.com>
2022-09-30 15:20:43 +0700

    Add files via upload

---
+++ B3_String/StringExercise.java
@@ -0,0 +1,146 @@
+import java.sql.Array;
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void addTwoString() {
+        String s1 = "An";
+        String s2 = "Nguyen";
+        String s3 = s1.concat(s2);
+        System.out.println(s3);
+    }
+
+    public static void changeToUpperCase() {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Nhap xau: ");
+        String str = kb.nextLine();
+        str = str.toUpperCase();
+        System.out.println("Xau in hoa: " + str);
+    }
+
+    public static void countUpperChar() {
+        int count = 0;
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Nhap xau:");
+        String str = kb.nextLine();
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isUpperCase(str.charAt(i))) {
+                count++;
+            }
+        }
+        System.out.println("So ki tu in hoa trong xau la: " + count);
+    }
+
+    public static void countDigit() {
+        int count = 0;
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Nhap xau:");
+        String str = kb.nextLine();
+        for (int i = 0; i < str.length(); i++) {
+            if(Character.isDigit(str.charAt(i))) {
+                count++;
+            }
+        }
+        System.out.println("So ki tu so trong xau la: " + count);
+    }
+
+    public static void printInOddIndex() {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Nhap xau: ");
+        String str = kb.nextLine();
+        System.out.println("Ki tu o vi tri le trong xau: ");
+        for (int i = 0; i < str.length(); i++) {
+            if (i % 2 == 0) {
+                System.out.println(str.charAt(i) + " ");
+            }
+        }
+    }
+
+    public static void printSpecialChar() {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Nhap xau: ");
+        String str = kb.nextLine();
+        System.out.println("Ki tu dac biet trong xau: ");
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isLetterOrDigit(str.charAt(i))) {
+                System.out.print(str.charAt(i) + " ");
+            }
+        }
+    }
+
+    public static void deleteSpace() {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Nhap xau: ");
+        String str = kb.nextLine();
+        str = str.trim();
+        System.out.println("Xau khong con khoang trang o dau va cuoi: " + str);
+    }
+
+    public static void countLowerChar() {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Nhap xau: ");
+        String str = kb.nextLine();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLowerCase(str.charAt(i))) {
+                count++;
+            }
+        }
+        System.out.println("So ki tu thuong trong xau la: " +count);
+    }
+
+    public static void cutString() {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Nhap xau: ");
+        String str = kb.nextLine();
+        str = str.substring(1);
+        System.out.println("Xau sao khi cat la: " + str);
+    }
+
+    public static void getDigit() {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Nhap xau:");
+        String str = kb.nextLine();
+        int count = 0;
+        int index = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                count++;
+            }
+        }
+        int a[] = new int[count];
+        for (int i = 0; i < count; i++) {
+            a[index] = Character.getNumericValue(str.charAt(i));
+            index++;
+        }
+        System.out.println("Mang so nguyen trong xau: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    public static void cutString2() {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Nhap xau: ");
+        String str = kb.nextLine();
+        str = str.substring(1,8);
+        System.out.println("Xau sao khi cat la: " + str);
+    }
+
+    public static void main(String[] args) {
+        addTwoString();
+        changeToUpperCase();
+        countUpperChar();
+        countDigit();
+        printInOddIndex();
+        printSpecialChar();
+        deleteSpace();
+        countLowerChar();
+        cutString();
+        cutString2();
+        getDigit();
+
+    }
+
+
+
+}
